cac59d42d1e8b86a6fec33014cddfe834eb50728
yufernando <yufernando@gmail.com>
2019-02-28 10:21:51 -0500

    Comment out git clone

--- setup.sh
+++ setup.sh
@@ -6,15 +6,16 @@ DIR=$PWD
 
 # Installation
 
-git clone git@github.com:yufernando/dotfiles.git
+# git clone git@github.com:yufernando/dotfiles.git
 
 # Create Symlinks
 
-ln -sf $DIR/bash_profile ~/.bash_profile
-ln -sf $DIR/init.vim     ~/.config/nvim/init.vim
-ln -sf $DIR/tmux.conf    ~/.tmux.conf
-ln -sf $DIR/vimrc        ~/.vimrc
-ln -sf $DIR/zshrc        ~/.zshrc
-ln -sf $DIR/vim/autocmds.vim ~/.vim/autoload/autocmds.vim        
+ln -svf $DIR/bash_profile ~/.bash_profile
+ln -svf $DIR/init.vim     ~/.config/nvim/init.vim
+ln -svf $DIR/tmux.conf    ~/.tmux.conf
+ln -svf $DIR/vimrc        ~/.vimrc
+ln -svf $DIR/zshrc        ~/.zshrc
+ln -svf $DIR/vim/autocmds.vim ~/.vim/autoload/autocmds.vim        
+ln -svf $DIR/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
 
 echo "Installation complete! Relogin please"

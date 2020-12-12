130b5af4ab64436be2f6aaa65195c0d223271ec7
yufernando <yufernando@gmail.com>
2020-12-12 00:23:18 -0500

    Move jupyter to compute.aretian

--- jupyterhub/jupyterhub_config-default.py
+++ jupyterhub/jupyterhub_config-default.py
@@ -1,4 +1,5 @@
-c.JupyterHub.bind_url = 'http://:8000/jupyter'
+c.JupyterHub.bind_url = 'http://:8000'
+# c.JupyterHub.bind_url = 'http://:8000/jupyter' # Previously served from /jupyter endpoint
 c.JupyterHub.cookie_secret_file = '/srv/jupyterhub/jupyterhub_cookie_secret'
 c.ConfigurableHTTPProxy.auth_token = '/srv/jupyterhub/proxy_auth_token'
 c.ConfigurableHTTPProxy.command = '/home/fer/miniconda3/bin/configurable-http-proxy'
@@ -6,3 +7,12 @@ c.Spawner.default_url = '/lab'
 c.Authenticator.admin_users = {'fer'}
 # Add user's conda environments to path: https://github.com/jupyterhub/jupyterhub/issues/2136#issuecomment-420642694
 # c.Spawner.cmd="/srv/jupyterhub/launch-script.sh"
+
+# Edit Login page: https://github.com/jupyterhub/jupyterhub/issues/1385#issuecomment-466178702
+# Comment following line to disable custom login page
+c.JupyterHub.template_paths=['/srv/jupyterhub']
+
+# Jupyter extension
+# import os
+# os.environ['JUPYTER_CONFIG_DIR'] = '/usr/local/etc/jupyter/nbconfig'
+# c.Spawner.env_keep.append('JUPYTER_CONFIG_DIR')

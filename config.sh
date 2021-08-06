405aba41a379c63d4d16e168267da50fa25d7b47
yufernando <yufernando@gmail.com>
2021-08-06 02:19:36 -0400

    Simplify config script
    Removes config of jupyterhub, nginx and other unused features for a base
    installation.

--- config.sh
+++ config.sh
@@ -30,26 +30,24 @@ ln -svf $DIR/vimrc                      ~/.vimrc
 ln -svf $DIR/init.vim                   ~/.config/nvim/init.vim
 ln -svf $DIR/vim/autocmds.vim           ~/.vim/autoload/autocmds.vim        
 ln -svf $DIR/tmux.conf                  ~/.tmux.conf
-ln -svf $DIR/tmux/tunes.js              ~/.tmux/tunes.js
 ln -svf $DIR/stata_kernel.conf          ~/.stata_kernel.conf
 ln -svf $DIR/latexmkrc                  ~/.latexmkrc
-ln -svf $DIR/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
-ln -svf $DIR/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/plugin.jupyterlab-settings
-ln -svf $DIR/settings.json              ~/Library/Application\Support/Code/User/settings.json
-ln -svf $DIR/karabiner.json             ~/.config/karabiner/karabiner.json
-ln -svf $DIR/thesis.sty                 ~/Library/texmf/tex/latex/local/thesis.sty
-ln -svf $DIR/oh-my-zsh/robbyrussell-userhost.zsh-theme  ~/.oh-my-zsh/custom/themes/robbyrussell-userhost.zsh-theme
+#ln -svf $DIR/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+#ln -svf $DIR/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/plugin.jupyterlab-settings
+#ln -svf $DIR/thesis.sty                 ~/Library/texmf/tex/latex/local/thesis.sty
 
-#Remote server
-sudo ln -s /home/fer/.dotfiles/zshrc    /root/.zshrc
-sudo ln -s /home/fer/.dotfiles/init.vim /root/.config/nvim/init.vim
-sudo ln -svf $DIR/nginx/ferserver       /etc/nginx/sites-enabled/ferserver
+# Root User
+
+sudo ln -svf /home/fer/.dotfiles/zshrc    /root/.zshrc
+sudo mkdir -p /root/.config/nvim
+sudo ln -svf /home/fer/.dotfiles/init.vim /root/.config/nvim/init.vim
+# sudo ln -svf $DIR/nginx/ferserver       /etc/nginx/sites-enabled/ferserver
 
 #Jupyterhub
-ln -svf $DIR/jupyterhub/jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
+#ln -svf $DIR/jupyterhub/jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
 #.dotfiles -> /srv -> /systemd
-sudo ln -svf $DIR/jupyterhub/jupyterhub.service /srv/jupyterhub/jupyterhub.service
-sudo ln -svf /srv/jupyterhub/jupyterhub.service /etc/systemd/system/jupyterhub.service  
+#sudo ln -svf $DIR/jupyterhub/jupyterhub.service /srv/jupyterhub/jupyterhub.service
+#sudo ln -svf /srv/jupyterhub/jupyterhub.service /etc/systemd/system/jupyterhub.service  
 
 # Italics in iTerm and Tmux
 # https://apple.stackexchange.com/a/267261

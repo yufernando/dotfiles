6d9c530a79d85c00501ffdd7a00e5f07aeeec537
yufernando <yufernando@gmail.com>
2021-08-06 02:20:32 -0400

    Simplify install script
    Updates install routines for Vim-Plug and Oh-my-zsh. Removes redundant
    installation of tmux and git.

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@
 
 # Install utilities
 #curl
-sudo apt install curl
+#sudo apt install curl
 #homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-sudo apt-get install build-essential
-echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/fer/.zprofile
-eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-brew install gcc
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#sudo apt-get install build-essential
+#echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> /home/fer/.zprofile
+#eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+#brew install gcc
 #install pip
 sudo apt install python3-pip
 
 # Install VIM
 sudo apt install neovim
 # Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install git
 sudo apt install git
@@ -27,11 +27,8 @@ sudo apt install git
 sudo apt install tmux
 
 # Install zsh (need to reboot to change shell)
-sudo apt install zsh -Y
+sudo apt --yes install zsh
 chsh -s /usr/bin/zsh
 
 # Install oh-my-zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

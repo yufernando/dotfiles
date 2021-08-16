9ca875fea3e7890d9be8616c14309db88433d7ca
yufernando <yufernando@gmail.com>
2021-08-16 14:41:08 -0400

    3_install: add curl

--- 3_install.sh
+++ 3_install.sh
@@ -13,7 +13,7 @@
 #brew install gcc
 
 # Install pip and neovim
-sudo apt -y install python3-pip neovim
+sudo apt -y install python3-pip neovim curl
 
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \

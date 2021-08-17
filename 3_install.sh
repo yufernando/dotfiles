5ed5925bc19d1cd6c3c5e900ed7f11d04ade20a5
yufernando <yufernando@gmail.com>
2021-08-16 20:29:37 -0400

    3_install: install vim-plug programatically

--- 3_install.sh
+++ 3_install.sh
@@ -14,7 +14,7 @@ sudo apt -y install python3-pip neovim curl git tmux zsh ripgrep
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim +PlugInstall +qall > /dev/null
+nvim --headless +PlugInstall +qall > /dev/null 2>&1
 
 # Change shell to zsh
 chsh -s /usr/bin/zsh

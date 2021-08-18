383db50777116f144cd73e7055a379ae20722b7c
yufernando <yufernando@gmail.com>
2021-08-17 23:13:13 -0400

    Organize files into folders

--- 2_config.sh
+++ 2_config.sh
@@ -25,17 +25,17 @@ touch ~/.zshrc
 
 ln -svf $DIR/bash_profile               ~/.bash_profile
 ln -svf $DIR/gitconfig                  ~/.gitconfig
-ln -svf $DIR/zshrc                      ~/.zshrc
-ln -svf $DIR/vim/vimrc                      ~/.vimrc
+ln -svf $DIR/zsh/zshrc                  ~/.zshrc
+ln -svf $DIR/vim/vimrc                  ~/.vimrc
 ln -svf $DIR/vim/init.vim               ~/.config/nvim/init.vim
 ln -svf $DIR/vim/autocmds.vim           ~/.vim/autoload/autocmds.vim        
 ln -svf $DIR/vim/after/ftplugin/python.vim  ~/.vim/after/ftplugin/python.vim
-ln -svf $DIR/tmux.conf                  ~/.tmux.conf
+ln -svf $DIR/tmux/tmux.conf             ~/.tmux.conf
 ln -svf $DIR/tmux/tunes.js              ~/.tmux/tunes.js
 ln -svf $DIR/stata_kernel.conf          ~/.stata_kernel.conf
 ln -svf $DIR/latexmkrc                  ~/.latexmkrc
-# ln -svf $DIR/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
-# ln -svf $DIR/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/plugin.jupyterlab-settings
+# ln -svf $DIR/jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+# ln -svf $DIR/jupyter/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/plugin.jupyterlab-settings
 ln -svf $DIR/settings.json              ~/Library/Application\Support/Code/User/settings.json
 ln -svf $DIR/karabiner.json             ~/.config/karabiner/karabiner.json
 ln -svf $DIR/thesis.sty                 ~/Library/texmf/tex/latex/local/thesis.sty

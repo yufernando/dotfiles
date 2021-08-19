378308a4ccf7ebbff9464e66945a7e1e5532687b
yufernando <yufernando@gmail.com>
2021-08-19 15:40:19 -0400

    Add user feedback

--- linode/deploy.sh
+++ linode/deploy.sh
@@ -17,22 +17,22 @@ echo "Reading configuration variables from .env file:"
 echo "HOSTNAME:        $HOSTNAME"
 echo "LABEL:           $LABEL"
 echo "USERNAME:        $USERNAME"
-echo "STACKSCRIPT_ID:  $STACKSCRIPT_ID"
-echo "REGION:          $REGION"
-echo "IMAGE:           $IMAGE"
-echo "PLAN:            $PLAN"
 if [[ -n $SSHKEY ]]; then
     echo "SSHKEY provided: yes"
 else
     echo "SSHKEY provided: no"
 fi
+echo "STACKSCRIPT_ID:  $STACKSCRIPT_ID"
+echo "REGION:          $REGION"
+echo "IMAGE:           $IMAGE"
+echo "PLAN:            $PLAN"
 echo ""
 
 # Create new linode
-# linode-cli linodes create --label "${LABEL}" \
-#                           --region "${REGION}" \
-#                           --stackscript_id "${STACKSCRIPT_ID}" \
-#                           --stackscript_data '{ "SCRIPT_HOSTNAME":"'"${HOSTNAME}"'", "SCRIPT_USERNAME":"'"${USERNAME}"'", "SCRIPT_PASSWORD":"'"${PASSWORD}"'", "SCRIPT_IGNOREIP":"'"${IGNOREIP}"'", "SCRIPT_SSHKEY":"'"${SSHKEY}"'" }' \
-#                           --image "${IMAGE}" \
-#                           --type "${PLAN}" \
-#                           --root_pass "${PASSWORD}"
+linode-cli linodes create --label "${LABEL}" \
+                          --region "${REGION}" \
+                          --stackscript_id "${STACKSCRIPT_ID}" \
+                          --stackscript_data '{ "SCRIPT_HOSTNAME":"'"${HOSTNAME}"'", "SCRIPT_USERNAME":"'"${USERNAME}"'", "SCRIPT_PASSWORD":"'"${PASSWORD}"'", "SCRIPT_IGNOREIP":"'"${IGNOREIP}"'", "SCRIPT_SSHKEY":"'"${SSHKEY}"'" }' \
+                          --image "${IMAGE}" \
+                          --type "${PLAN}" \
+                          --root_pass "${PASSWORD}"

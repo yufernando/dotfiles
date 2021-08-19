2c312a521935cc103afa25213a4d0dca840ab049
yufernando <yufernando@gmail.com>
2021-08-19 14:12:30 -0400

    deploy.sh: fix JSON format bug

--- linode/deploy.sh
+++ linode/deploy.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+echo "Creating new Linode."
+read -p "Hostname: " HOSTNAME
+read -s -p "Password: " PASSWORD
+echo ""
+echo "Reading configuration variables from .env file."
+
+LABEL=$HOSTNAME
+
+# Import environment variables
 set -o allexport
 source .env
 set +o allexport
 
+# Create new linode
 linode-cli linodes create --label "${LABEL}" \
                           --region "${REGION}" \
-                          --root_pass "${PASSWORD}" \
                           --stackscript_id "${STACKSCRIPT_ID}" \
-                          --stackscript_data '{ "HOST":"$HOST", "USERNAME":"$USERNAME", "PASSWORD":"$PASSWORD", "IGNOREIP":"$IGNOREIP", "SSHKEY":"$SSHKEY"}' \
+                          --stackscript_data '{ "SCRIPT_HOSTNAME":"'"${HOSTNAME}"'", "SCRIPT_USERNAME":"'"${USERNAME}"'", "SCRIPT_PASSWORD":"'"${PASSWORD}"'", "SCRIPT_IGNOREIP":"'"${IGNOREIP}"'", "SCRIPT_SSHKEY":"'"${SSHKEY}"'" }' \
                           --image "${IMAGE}" \
-                          --type "${PLAN}"
+                          --type "${PLAN}" \
+                          --root_pass "${PASSWORD}"

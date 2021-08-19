fda53714f48fed06651201e5a09d896dd3c19065
yufernando <yufernando@gmail.com>
2021-08-18 23:34:01 -0400

    0_setup: update and upgrade noninteractive

--- 0_setup.sh
+++ 0_setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #
 # GENERAL CONFIGURATION (0_setup.sh)
-# Configure hostname, ip and date
+# Update, upgrade, configure hostname, ip and date
 #----------------------------------------------------------------
 echo ""
-echo "SETUP: configuring hostname, ip and date."
+echo "SETUP: Update, upgrade, configure hostname, ip and date."
 echo ""
+
+# Update system over IPv4 without any interaction
+apt-get -o Acquire::ForceIPv4=true update
+DEBIAN_FRONTEND=noninteractive \
+  apt-get \
+  -o Dpkg::Options::=--force-confold \
+  -o Dpkg::Options::=--force-confdef \
+  -y --allow-downgrades --allow-remove-essential --allow-change-held-packages
 
 # Get Hostname
 HOSTNAME=$1
@@ -15,8 +23,6 @@ IP_ADDRESS=$(ip address show eth0 | grep "inet " | awk '{print $2}' | cut -d '/'
 IPV6_ADDRESS=$(ip address show eth0 | grep -m1 "inet6 " | awk '{print $2}' | cut -d '/' -f1)
 
 # Set Hostname
-apt update
-apt dist-upgrade -y
 hostnamectl set-hostname $HOSTNAME
 echo "$IP_ADDRESS	$HOSTNAME" >> /etc/hosts
 echo "$IPV6_ADDRESS	$HOSTNAME" >> /etc/hosts

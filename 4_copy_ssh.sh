10fc2612fd569e821ecbb4e67559be0da4dc39ab
yufernando <yufernando@gmail.com>
2021-08-19 00:16:08 -0400

    4_copy_ssh: remove sudo

--- 4_copy_ssh.sh
+++ 4_copy_ssh.sh
@@ -16,7 +16,7 @@ SSHKEY=$2
 if [[ -z $SSHKEY ]]; then
     if [[ -e /root/.ssh/authorized_keys ]]; then
         mkdir -p /home/$USER/.ssh
-        sudo cp /root/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys
-        sudo chown $USER:$USER /home/$USER/.ssh/authorized_keys
+        cp /root/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys
+        chown $USER:$USER /home/$USER/.ssh/authorized_keys
     fi
 fi

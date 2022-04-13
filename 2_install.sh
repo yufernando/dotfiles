3ed65c2f925664224c29b9de4e58e75c7db2cda9
yufernando <yufernando@gmail.com>
2022-04-13 16:25:30 -0400

    rebase: prepare install detecting operating system

--- 2_install.sh
+++ 2_install.sh
@@ -10,7 +10,7 @@ echo ""
 
 UNAME=$(uname)
 
-if [[ "$UNAME" == "Linux" ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
 # Update and upgrade
 DEBIAN_FRONTEND=noninteractive apt-get update && apt-get -y upgrade
@@ -45,7 +45,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
-if [[ "$UNAME" == "Darwin" ]]; then
+if [[ "$OSTYPE" == "darwin"* ]]; then
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 

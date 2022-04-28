2201a9a67c37ea247b48aef3e8f4ead65b150b2c
yufernando <yufernando@gmail.com>
2022-04-28 10:30:38 -0400

    fix(condarc): remove strict channel priority
    Strict channel priority does not allow to properly install bioconda
    packages (e.g. snakemake) after building a docker image with this
    setting.

--- 3_config.sh
+++ 3_config.sh
@@ -27,38 +27,28 @@ ln -svf $DIR/bash_profile               ~/.bash_profile
 ln -svf $DIR/gitconfig                  ~/.gitconfig
 ln -svf $DIR/zsh/zshrc                  ~/.zshrc
 if [[ "$OSTYPE" == "darwin"* ]]; then
-ln -svf $DIR/zsh/zprofile               ~/.zprofile #Ruby, Rust and pyenv
+    ln -svf $DIR/zsh/zprofile           ~/.zprofile #Ruby, Rust and pyenv
 fi
 
 ln -svf $PWD/oh-my-zsh/robbyrussell-userhost.zsh-theme  ~/.oh-my-zsh/custom/themes/robbyrussell-userhost.zsh-theme
 ln -svf $DIR/stata_kernel.conf          ~/.stata_kernel.conf
 ln -svf $DIR/latexmkrc                  ~/.latexmkrc
-#ln -svf $DIR/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
-#ln -svf $DIR/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/plugin.jupyterlab-settings
-
-# sudo ln -svf $DIR/nginx/ferserver       /etc/nginx/sites-enabled/ferserver
-
-#Jupyterhub
-#ln -svf $DIR/jupyterhub/jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
-#.dotfiles -> /srv -> /systemd
-#sudo ln -svf $DIR/jupyterhub/jupyterhub.service /srv/jupyterhub/jupyterhub.service
-#sudo ln -svf /srv/jupyterhub/jupyterhub.service /etc/systemd/system/jupyterhub.service  
 
 # Tmux
 if [[ "$OSTYPE" == "darwin"* ]]; then
-ln -svf $DIR/tmux/tmux.conf.mac             ~/.tmux.conf
-ln -svf $DIR/tmux/tunes.js              ~/.tmux/tunes.js
+    ln -svf $DIR/tmux/tmux.conf.mac     ~/.tmux.conf
+    ln -svf $DIR/tmux/tunes.js          ~/.tmux/tunes.js
 fi
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-ln -svf $DIR/tmux/tmux.conf.ubuntu          ~/.tmux.conf
+    ln -svf $DIR/tmux/tmux.conf.ubuntu  ~/.tmux.conf
 fi
 
 # Italics in iTerm and Tmux
 # https://apple.stackexchange.com/a/267261
 if [[ "$OSTYPE" == "darwin"* ]]; then
-tic -o ~/.terminfo $DIR/tmux/tmux-256color.terminfo_mac.txt || true
+    tic -o ~/.terminfo $DIR/tmux/tmux-256color.terminfo_mac.txt || true
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-tic -o ~/.terminfo $DIR/tmux/tmux-256color.terminfo_ubuntu.txt || true
+    tic -o ~/.terminfo $DIR/tmux/tmux-256color.terminfo_ubuntu.txt || true
 fi
 tic -o ~/.terminfo $DIR/tmux/xterm-256color.terminfo.txt || true
 tic -o ~/.terminfo $DIR/tmux/tmux.terminfo.txt || true
@@ -67,15 +57,21 @@ tic -o ~/.terminfo $DIR/tmux/tmux.terminfo.txt || true
 ln -svf $DIR/ranger/rc.conf             ~/.config/ranger/rc.conf
 ln -svf $DIR/ranger/commands.py         ~/.config/ranger/commands.py
 ln -svf $DIR/vifmrc                     ~/.config/vifm/vifmrc
-ln -svf $DIR/conda/condarc              ~/.condarc
+
+# Conda
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ln -svf $DIR/conda/condarc          ~/.condarc
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ln -svf $DIR/conda/condarc_linux    ~/.condarc
+fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-# VSCode
-ln -svf $DIR/settings.json              ~/Library/Application\Support/Code/User/settings.json
-# Karabiner
-ln -svf $DIR/karabiner.json             ~/.config/karabiner/karabiner.json
-# Latex
-ln -svf $DIR/thesis.sty                 ~/Library/texmf/tex/latex/local/thesis.sty
+    # VSCode
+    ln -svf $DIR/settings.json          ~/Library/Application\Support/Code/User/settings.json
+    # Karabiner
+    ln -svf $DIR/karabiner.json         ~/.config/karabiner/karabiner.json
+    # Latex
+    ln -svf $DIR/thesis.sty             ~/Library/texmf/tex/latex/local/thesis.sty
 fi
 
 # Vim
@@ -86,3 +82,16 @@ ln -svf $DIR/vim/autocmds.vim           ~/.vim/autoload/autocmds.vim
 # Install Vim Plugins
 echo "Installing Vim Plugins..."
 nvim --headless +PlugInstall +UpdateRemotePlugins +qall > /dev/null 2>&1
+
+# EXTRA
+
+# Jupyter
+#ln -svf $DIR/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+#ln -svf $DIR/plugin.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/shortcuts-extension/plugin.jupyterlab-settings
+
+#Jupyterhub
+#sudo ln -svf $DIR/nginx/ferserver       /etc/nginx/sites-enabled/ferserver
+#ln -svf $DIR/jupyterhub/jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
+#.dotfiles -> /srv -> /systemd
+#sudo ln -svf $DIR/jupyterhub/jupyterhub.service /srv/jupyterhub/jupyterhub.service
+#sudo ln -svf /srv/jupyterhub/jupyterhub.service /etc/systemd/system/jupyterhub.service  

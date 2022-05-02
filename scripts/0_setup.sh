a12bc5d149e88230f7c05f4bbe8a648079d7eb5e
yufernando <yufernando@gmail.com>
2022-05-02 15:55:54 -0400

    fix(0_harden): avoid using systemd for Docker containers

--- scripts/0_setup.sh
+++ scripts/0_setup.sh
@@ -19,18 +19,18 @@ DEBIAN_FRONTEND=noninteractive \
 # Get Hostname
 SET_HOSTNAME=$1
 
-# Get IP Addresses
-IP_ADDRESS=$(ip address show eth0 | grep "inet " | awk '{print $2}' | cut -d '/' -f1)
-IPV6_ADDRESS=$(ip address show eth0 | grep -m1 "inet6 " | awk '{print $2}' | cut -d '/' -f1)
-
 # Set Hostname and time
-if [ -f ~/.docker-date-created ]; then
-    awk 'NR==1 {$0=$SET_HOSTNAME} {print} /etc/hostname'
-else
+if [ ! -f ~/.docker-date-created ]; then
+    # Get IP Addresses
+    IP_ADDRESS=$(ip address show eth0 | grep "inet " | awk '{print $2}' | cut -d '/' -f1)
+    IPV6_ADDRESS=$(ip address show eth0 | grep -m1 "inet6 " | awk '{print $2}' | cut -d '/' -f1)
+    echo "$IP_ADDRESS	$SET_HOSTNAME" >> /etc/hosts
+    echo "$IPV6_ADDRESS	$SET_HOSTNAME" >> /etc/hosts
+    # Set hostname
     hostnamectl set-hostname $SET_HOSTNAME
+    # Set timezone
     timedatectl set-timezone "America/New_York"
+else
+    # Docker container
+    awk 'NR==1 {$0=$SET_HOSTNAME} {print} /etc/hostname'
 fi
-
-echo "$IP_ADDRESS	$SET_HOSTNAME" >> /etc/hosts
-echo "$IPV6_ADDRESS	$SET_HOSTNAME" >> /etc/hosts
-
